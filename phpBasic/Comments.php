b6e4ca2b2716134c2f84f5ca0df922af476c255d
KlausTeppler <38588858+KlausTeppler@users.noreply.github.com>
2019-08-27 16:08:55 +0200

    php comments

---
+++ phpBasic/Comments.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<?php
+
+
+//Drei Möglichkeiten um Kommentare im php-code zu verwenden
+
+
+
+#Perl Style Comments Single Line
+
+
+
+
+// C Style Comments Single Line
+
+
+
+
+/*
+    Multiline Comments
+    More than 
+    one line
+
+*/
+
+
+?>
+
+
+<!-- Was passier in Verbindung mit HTML? -->
+
+
+<!-- das wird ein Kommentar in dem php verwendet wird
+<?php echo "php war das !!!"?>
+und hier geht der Kommentar weiter!!! -->
+
+
+</body>
+</html>

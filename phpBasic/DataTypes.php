b49414769c5f17af23b6eaee3b5a4cf2abe4f622
Klaus Teppler <kteppler@gmail.com>
2019-10-07 11:46:31 +0200

    add dataTypes

---
+++ phpBasic/DataTypes.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Using Scalar Data Types,
+ * Able to define variables using:
+ * Integers, boolean, doubles, Strings and null.
+ * 
+ */
+
+ /* Scalar are data types that has a single value.
+    4 types in php:
+    - Boolean
+    - Integer
+    - Float
+    - String
+ */
+
+ /**
+  *  Integers are whole numbers,
+  *  + or - values,
+  *  can hold Octal(base 8), Hexadecimal(base 16) and Binary(base 2) values,
+  *  every plattform will have limited integer number to be defined
+  *  Constant PHP_INT_SIZE shows the size of in Integer in bytes
+  *  Constant PHP_INT_MAX show the max size of a positiv integer
+  */
+
+echo PHP_INT_SIZE.PHP_EOL;
+$value = 1;
+  
